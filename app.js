2ae90b059528613fa18672e5a05e33621d8a2769
Danh Nguyen <danhnguyen267@gmail.com>
2018-03-30 10:37:03 -0700

    Added path var to resolve error

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+const path = require('path');
 const mysql = require('mysql');
 const flash = require('express-flash');
 const cookieParser = require('cookie-parser');
